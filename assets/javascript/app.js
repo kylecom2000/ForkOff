699c45b6cfe52d685e92315ac181709b5053ae08
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2018-11-08 11:47:27 -0600

    Really finally did add ID lists and API keys to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,18 @@ var config = {
     messagingSenderId: "221859996139"
   };
   firebase.initializeApp(config);
+
+// ID for main container: main-content
+// ID for start button: start-btn
+// ID for thumbs up, thumbs down: thumbs-up, thumbs-down
+// ID for loading image: loading-img
+// ID for restaurant icon: food-img
+// ID for winning restaurant image: result-img
+// ID for forks that spin: fork-right, fork-left
+// ID for final page: results
+// ID for text on that page: results-directions
+// Class of thumb buttons: thumbs
+//
+
+var GoogleAPIkey = "AIzaSyA7b0Y8wH7Awthb9-CDlqAPtrr-Q2JCTVw";
+var ZomatoAPIkey = "c30eca16c0c03ef51799b26e942490e3";
